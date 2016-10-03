a4a26cf59c7e38b00db57cffb225ebebceeb8af7
Ashwin Srinath <atrikut@clemson.edu>
2016-10-03 09:57:01 -0400

    Add test cases and update find_zero

--- data/find_zero.py
+++ data/find_zero.py
@@ -1,11 +1,43 @@
 import numpy as np
 
-def find_zero_rows(filename):
+def count_zero_rows(filename):
+    """
+    Counts the number of rows
+    containing all zeros
+    in the file "filename"
+
+    Parameters
+    ----------
+
+    filename : str
+        Name of csv file containing the data
+
+    Returns
+    -------
+
+    num_zero_rows : int
+        Number of zero rows
+        
+    Examples
+    --------
+    >>> count_zero_rows('small-01.csv')
+    2
+
+    >>> count_zero_rows('small-02.csv')
+    0
+
+    """
+
+    # read the file
     data = np.loadtxt(filename, delimiter=',')
-    num_zero_rows = 0
+
+    # get number of columns and rows:
     cols, rows = np.shape(data)
+
+    # loop over rows; increase num_zero_rows if any row is all zeros
+    num_zero_rows = 0
     for i in range(rows):
         if np.all(data[i, :]==0):
             num_zero_rows = num_zero_rows + 1
-    return num_zero_rows
 
+    return num_zero_rows
